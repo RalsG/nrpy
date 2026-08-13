20569f7bfe3e9df9c91e54e694cce95f41795f11
RalsG <ralston.graves@gmail.com>
2026-08-13 13:15:35 -0700

    Rename project from 'spinning_blackhole2' to 'spinning_blackhole'
    Accidentally left this change - used for running multiple examples at once

--- nrpy/examples/spinning_blackhole.py
+++ nrpy/examples/spinning_blackhole.py
@@ -56,7 +56,7 @@ par.set_parval_from_str("parallelization", parallelization)
 par.set_parval_from_str("fp_type", fp_type)
 
 # Code-generation-time parameters:
-project_name = "spinning_blackhole2"
+project_name = "spinning_blackhole"
 CoordSystem = "SinhCylindrical"
 IDtype = "UIUCBlackHole"
 # IDtype = "OffsetKerrSchild"
